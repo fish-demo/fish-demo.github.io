e48bcbfc4e89d86b8b2efbca5e621e6633adef12
dilwyn.queshire <dilwyn.queshire@i.ua>
2018-04-15 13:57:46 +0300

    Added background cover

--- Scene.js
+++ Scene.js
@@ -25,9 +25,8 @@ var Scene = function () {
         var elapsedTime = timeStamp - prevAnimationFrameTimeStamp;
         prevAnimationFrameTimeStamp = timeStamp;
         
-        context.clearRect(0, 0, width, height);
+        drawImageProp(context, sprites.get("background")[0], 0, 0, width, height);
 
-        context.drawImage(sprites.get("background")[0], 0, 0, width, height);
         var maxDimension = Math.max(width, height);
 
         var i;
@@ -82,4 +81,59 @@ var Scene = function () {
             window.requestAnimationFrame(renderAnimationFrame);
         });
     };
+
+     /**
+     * By Ken Fyrstenberg Nilsen
+     *
+     * drawImageProp(context, image [, x, y, width, height [,offsetX, offsetY]])
+     *
+     * If image and context are only arguments rectangle will equal canvas
+    */
+    function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
+    
+        if (arguments.length === 2) {
+            x = y = 0;
+            w = ctx.canvas.width;
+            h = ctx.canvas.height;
+        }
+    
+        /// default offset is center
+        offsetX = typeof offsetX === 'number' ? offsetX : 0.5;
+        offsetY = typeof offsetY === 'number' ? offsetY : 0.5;
+    
+        /// keep bounds [0.0, 1.0]
+        if (offsetX < 0) offsetX = 0;
+        if (offsetY < 0) offsetY = 0;
+        if (offsetX > 1) offsetX = 1;
+        if (offsetY > 1) offsetY = 1;
+    
+        var iw = img.width,
+            ih = img.height,
+            r = Math.min(w / iw, h / ih),
+            nw = iw * r,   /// new prop. width
+            nh = ih * r,   /// new prop. height
+            cx, cy, cw, ch, ar = 1;
+    
+        /// decide which gap to fill    
+        if (nw < w) ar = w / nw;
+        if (nh < h) ar = h / nh;
+        nw *= ar;
+        nh *= ar;
+    
+        /// calc source rectangle
+        cw = iw / (nw / w);
+        ch = ih / (nh / h);
+    
+        cx = (iw - cw) * offsetX;
+        cy = (ih - ch) * offsetY;
+    
+        /// make sure source rectangle is valid
+        if (cx < 0) cx = 0;
+        if (cy < 0) cy = 0;
+        if (cw > iw) cw = iw;
+        if (ch > ih) ch = ih;
+    
+        /// fill image in dest. rectangle
+        ctx.drawImage(img, cx, cy, cw, ch,  x, y, w, h);
+    }   
 };
